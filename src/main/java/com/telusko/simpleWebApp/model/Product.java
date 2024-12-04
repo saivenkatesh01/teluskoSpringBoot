99addfde114831fe379937c447ce8f0d6c734393
saivenkatesh01 <64096655+saivenkatesh01@users.noreply.github.com>
2024-12-04 15:27:03 +0530

    500 internal error
    Added method that converts json to required object. It may occur if there are any issues with jackson annotation of web annotation.

--- src/main/java/com/telusko/simpleWebApp/model/Product.java
+++ src/main/java/com/telusko/simpleWebApp/model/Product.java
@@ -3,9 +3,12 @@ package com.telusko.simpleWebApp.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.stereotype.Component;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Product {
 
     private int prodId;
@@ -20,4 +23,15 @@ public class Product {
                 ", price=" + price +
                 '}';
     }
+
+    // Use this method if json is not getting converted to object type. It may occur if any issues at jackson annotation. will get 500 error code
+    //@JsonCreator
+    //public User(@JsonProperty("prodId") int prodId,@Jsonproperty("prodName") String prodName, @Jsonproperty("price") int price){
+    //    this.prodId=prodId;
+    //    this.prodName=prodName;
+    //    this.price=price;
+    //}
+        
+
+    
 }
